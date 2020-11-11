ceeee12e6e646a187aff53e0b0249c94c0f5b42f
Sergey A. Rodionov <rsa_inc@mail.ru>
2020-11-11 15:36:43 +0400

    Service_Type to add #1

---
+++ modules/global/src/com/company/kvartplatomer/entity/ServiceType.java
@@ -0,0 +1,35 @@
+package com.company.kvartplatomer.entity;
+
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "KVARTPLATOMER_SERVICE_TYPE")
+@Entity(name = "kvartplatomer_ServiceType")
+public class ServiceType extends StandardEntity {
+    private static final long serialVersionUID = 1012134599303266995L;
+
+    @Column(name = "STYPE_NAME")
+    private String stypeName;
+
+    @Column(name = "SHORT_NAME")
+    private String shortName;
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getStypeName() {
+        return stypeName;
+    }
+
+    public void setStypeName(String stypeName) {
+        this.stypeName = stypeName;
+    }
+}
